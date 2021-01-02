b3bf50dd9e6bd5dc70ca514eb4a35ccfe882eb52
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-01-02 09:30:08 -0600

    Update plugin

--- src/main/java/net/countercraft/movecraft/repair/MovecraftRepair.java
+++ src/main/java/net/countercraft/movecraft/repair/MovecraftRepair.java
@@ -3,7 +3,6 @@ package net.countercraft.movecraft.repair;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MovecraftRepair extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
