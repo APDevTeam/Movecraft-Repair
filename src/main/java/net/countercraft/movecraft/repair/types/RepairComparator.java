7cd6aeff128e6a534e2e0b877ca88f81c6c492e7
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-09-05 10:20:30 -0500

    prototype comparator

---
+++ src/main/java/net/countercraft/movecraft/repair/types/RepairComparator.java
@@ -0,0 +1,11 @@
+package net.countercraft.movecraft.repair.types;
+
+import java.util.Comparator;
+
+public class RepairComparator implements Comparator<RepairTask> {
+
+    @Override
+    public int compare(RepairTask first, RepairTask second) {
+        return 0; // TODO
+    }
+}
