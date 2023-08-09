69e3d1822d4e1924f73ba89d42089f6d47be7b9e
goodroach <hughishikawa@gmail.com>
2023-08-08 23:46:06 -1000

    fix localization

--- src/main/java/net/countercraft/movecraft/repair/RepairListCommand.java
+++ src/main/java/net/countercraft/movecraft/repair/RepairListCommand.java
@@ -10,27 +10,27 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
+import static net.countercraft.movecraft.repair.util.WEUtils.SCHEMATIC_FORMAT;
 import static net.countercraft.movecraft.util.ChatUtils.MOVECRAFT_COMMAND_PREFIX;
 
 public class RepairListCommand implements CommandExecutor {
-    private int ENTRIES_PER_PAGE = 10;
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage(I18nSupport.getInternationalisedString(MOVECRAFT_COMMAND_PREFIX + "RepairList - This command can only be run by a player"));
+            sender.sendMessage("Repair - Must Be Player");
         }
         Player player = (Player) sender;
 
         if (!player.hasPermission("movecraft.repair.repairlist")) {
-            player.sendMessage(I18nSupport.getInternationalisedString(MOVECRAFT_COMMAND_PREFIX + "You don't have permission to use this command."));
+            player.sendMessage(I18nSupport.getInternationalisedString(MOVECRAFT_COMMAND_PREFIX + "Insufficient Permissions"));
             return true;
         }
 
         File repairDirectory = new File(MovecraftRepair.getInstance().getDataFolder(), "RepairStates");
         File playerDirectory = new File(repairDirectory, player.getUniqueId().toString());
         if (!playerDirectory.exists()) {
-            player.sendMessage(I18nSupport.getInternationalisedString(MOVECRAFT_COMMAND_PREFIX + "Repair States - None Found"));
+            player.sendMessage(I18nSupport.getInternationalisedString(MOVECRAFT_COMMAND_PREFIX + "Repair - None Found"));
             return true;
         }
 
@@ -47,27 +47,17 @@ public class RepairListCommand implements CommandExecutor {
             }
         }
 
-        int totalPages = (int) Math.ceil((double) schemList.length / ENTRIES_PER_PAGE);
-
-        if (page < 1 || page > totalPages) {
-            player.sendMessage(I18nSupport.getInternationalisedString("Error - Page number out of range."));
+        if (pageinator.isInBounds(page)) {
+            player.sendMessage(MOVECRAFT_COMMAND_PREFIX + I18nSupport.getInternationalisedString("Paginator - Invalid page") + "\"" + page + "\"");
             return true;
         }
 
-        int startIndex = (page - 1) * ENTRIES_PER_PAGE;
-        int endIndex = Math.min(startIndex + ENTRIES_PER_PAGE, schemList.length);
-
-        for (int i = startIndex; i < endIndex; i++) {
-            pageinator.addLine(schemList[i].getName().replace(".schem", ""));
+        for (File schemFile : schemList) {
+            pageinator.addLine(schemFile.getName().replace((CharSequence) SCHEMATIC_FORMAT, ""));
         }
 
         if (pageinator.isEmpty()) {
-            player.sendMessage(MOVECRAFT_COMMAND_PREFIX + I18nSupport.getInternationalisedString("Repair States - None Found"));
-            return true;
-        }
-
-        if(!pageinator.isInBounds(page)){
-            player.sendMessage(MOVECRAFT_COMMAND_PREFIX + I18nSupport.getInternationalisedString("Paginator - Invalid page") + "\"" + page + "\"");
+            player.sendMessage(MOVECRAFT_COMMAND_PREFIX + I18nSupport.getInternationalisedString("Repair - None Found"));
             return true;
         }
 
