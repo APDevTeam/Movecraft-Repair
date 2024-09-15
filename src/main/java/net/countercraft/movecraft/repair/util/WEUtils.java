07960c97b384d188efc625beb61fbac62ac4d7c8
TylerS1066 <TylerS1066@users.noreply.github.com>
2024-09-15 10:48:20 -0500

    Revert "Update WEUtils.java"
    This reverts commit 7868bd88f2700c7c4122a844feaee90e758f63e9.

--- src/main/java/net/countercraft/movecraft/repair/util/WEUtils.java
+++ src/main/java/net/countercraft/movecraft/repair/util/WEUtils.java
@@ -50,7 +50,7 @@ import net.countercraft.movecraft.util.hitboxes.HitBox;
 import net.countercraft.movecraft.util.hitboxes.SolidHitBox;
 
 public class WEUtils {
-    public static final ClipboardFormat SCHEMATIC_FORMAT = BuiltInClipboardFormat.SPONGE_SCHEMATIC;
+    public static final ClipboardFormat SCHEMATIC_FORMAT = BuiltInClipboardFormat.SPONGE_V2_SCHEMATIC;
 
     /**
      * Load a schematic from disk
